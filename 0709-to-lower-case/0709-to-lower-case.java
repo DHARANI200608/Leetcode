1e6c89b336580c072d53fe7ee0be925cad4a544e
DHARANI200608 <dharanikanagaraj2006@gmail.com>
2026-08-24 21:25:29 +0530

    Time: 0 ms (100%), Space: 43.2 MB (19.16%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,11 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String result = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            result = result + Character.toLowerCase(s.charAt(i));
-        }
-
-        return result;
+        return s.toLowerCase();
     }
 }
